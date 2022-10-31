7fcfbe500ae65a4843b5e015b53e1b0fa3142d26
Bryan Lojan <bryanlc9808@gmail.com>
2022-10-31 13:32:06 +0100

    url

--- src/services/beers.js
+++ src/services/beers.js
@@ -1,22 +1,21 @@
-let url = process.env.REACT_APP_API_URL;
+let url = "https://api.punkapi.com/v2/beers";
 export const getBeers = async () => {
-
-    const beers = await fetch(url)
-    .then(response => response.json())
-        .catch(error => []);
-    return beers;
-}
+  const beers = await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => []);
+  return beers;
+};
 
 export const filterBeer = async (beerName) => {
-    const filteredBeers = await fetch(`${url}?beer_name=${beerName}`)
-        .then(response => response.json())
-        .catch(error => []);
-    return filteredBeers;
-}
+  const filteredBeers = await fetch(`${url}?beer_name=${beerName}`)
+    .then((response) => response.json())
+    .catch((error) => []);
+  return filteredBeers;
+};
 
-export const getOneBeer = async (beerId) =>{
-    const oneBeer = await fetch(`${url}/${beerId}`)
-    .then(response => response.json())
-    .catch(error => []);
-return oneBeer;
-}
+export const getOneBeer = async (beerId) => {
+  const oneBeer = await fetch(`${url}/${beerId}`)
+    .then((response) => response.json())
+    .catch((error) => []);
+  return oneBeer;
+};
